cdb16e711a13c2c0bc3573ea849c63f9b35b5abd
hserykava <hserykava@ucsd.edu>
2025-06-03 02:28:02 -0700

    Update global.js

--- global.js
+++ global.js
@@ -226,7 +226,7 @@ function animateSmokyPath(startLength, endLength) {
   
     let shipTop = point.y + graphRect.top - ship.offsetHeight / 2;
     if (dy < 0) {
-      shipTop -=90;
+      shipTop -=70;
     }
 
     ship.style.left = `${shipLeft}px`;
